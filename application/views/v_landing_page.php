5506ab463b861d42d85de26c1b40845281b14912
King <stking@cm077-03.gateway.pace.com>
2014-12-20 22:56:42 -0500

    update marketing pages

--- application/views/v_landing_page.php
+++ application/views/v_landing_page.php
@@ -135,7 +135,7 @@
         	<div class="sync_text_holder">
             	<div class="sync_text_holder_heading">Film Sync App.</div>
             	<div class="text">
-                    FilmSync is a synchronized second screen app to be used during movies, documentary films, news video, live presentations and other linear experience. FilmSync delivers an audience engagement experience in sync with the story allowing viewers to passively or actively consume additional, unique content as the story progresses, making movie-watching a more interactive experience. 
+                    FilmSync is a synchronized second screen app to be used during movies, documentary films, news video, live presentations and other linear experiences. FilmSync delivers an audience engagement experience in sync with the story allowing viewers to passively or actively consume additional, unique content as the story progresses, making movie-watching a more interactive experience. 
                     <br><br>
                     Viewers can engage with the content in many ways, including: interactive maps and timelines, enhanced graphics, data visualizations, satellite imagery, trivia and even interact with other audience members viewing the film.
     
